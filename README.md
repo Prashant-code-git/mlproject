5775bd6e2c71e5452c75cbd666bee57989cca6f2
Prashant Singh <Prashant.2.Singh@coforge.com>
2024-06-05 11:40:08 +0530

    project str

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ## End to End Data Science Project ##
 
+Pipeline 
+1 training pipeline
+2 predication pipeline 
+
+Data Socrce ---> Data Ingestion --> Data Transformations ---> Model Traning --> Model MOitoring 
+-MySQL           -Train test Split  -compinents 
---
+++ templete.py
@@ -0,0 +1,39 @@
+import os 
+from pathlib import path 
+import logging 
+
+logging.basicConfig(level=logging.INFO)
+project_name ="mlproject"
+list_of_file=[
+#".github/workflows/.gitkeep",
+f"src/{project_name}/__init__.py",
+f"src/{project_name}/components/__init__.py",
+f"src/{project_name}/components/data_ingestion.py",
+f"src/{project_name}/components/data_tranformation.py",
+f"src/{project_name}/components/model_tranier.py",
+f"src/{project_name}/components/model_monitering.py",
+f"src/{project_name}/pipelines/__init__.py",
+f"src/{project_name}/pipelines/training_pipeline.py",
+f"src/{project_name}/pipelines/predication_pipeline.py",
+f"src/{project_name}/exception.py",
+f"src/{project_name}/logger.py",
+f"src/{project_name}/utils.py",
+"apps.py",
+"dockerfile",
+]
+
+for filepath in list_of_files:
+    filepath=path(filepath)
+    filepath,filename=os.path.split(filepath)
+
+    if filedir !="":
+        os.maredir(filedir,exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for thr file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file:{filepath}")
+
+    else:
+        logging.info(f"{filename} is alreday exists")
